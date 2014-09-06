83899a89b8dc2cb436432def068dd4b98086b009
luzexi <jesse_luzexi@163.com>
2014-09-07 00:26:42 +0800

    fix constan

--- cocos2d-html5-v2.2.3/memoryfruit/main.js
+++ cocos2d-html5-v2.2.3/memoryfruit/main.js
@@ -29,8 +29,8 @@ var cocos2dApp = cc.Application.extend({
     ctor:function (scene) {
         this._super();
         this.startScene = scene;
-        //cc.COCOS2D_DEBUG = this.config['COCOS2D_DEBUG'];
-        //cc.initDebugSetting();
+        cc.COCOS2D_DEBUG = this.config['COCOS2D_DEBUG'];
+        cc.initDebugSetting();
         cc.setup(this.config['tag']);
         cc.AppController.shareAppController().didFinishLaunchingWithOptions();
     },
@@ -73,7 +73,7 @@ var cocos2dApp = cc.Application.extend({
         cc.EGLView.getInstance().resizeWithBrowserSize(true);
 
         // turn on display FPS
-        //director.setDisplayStats(this.config['showFPS']);
+        director.setDisplayStats(this.config['showFPS']);
 
         // set FPS. the default value is 1.0/60 if you don't call this
         director.setAnimationInterval(1.0 / this.config['frameRate']);
